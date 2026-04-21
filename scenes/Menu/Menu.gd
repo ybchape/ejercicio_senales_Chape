df8fc61669f307b3f1b13978bc7fcab7481601dc
ybchape <yamilachl18@gmail.com>
2026-04-21 00:56:19 -0300

    Se creo una funcion para actualizar el contador cuando se resetea
    Se creo una función llamada delete, la cual resetea el contador y el texto, además se conecto la señal del botón reset a la escena menú.

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,6 +8,8 @@ var instancesCount = 0
 @onready var emisorred = $RedGenerator
 @onready var emisorviolet = $VioletGenerator
 
+@onready var emisorreset = $Reset
+
 func _ready():
 
 	# Inicializa el texto de "CountLabel" usando el texto
@@ -17,8 +19,14 @@ func _ready():
 	emisorred.button_down.connect(_count_new_instance)
 	emisorviolet.button_down.connect(_count_new_instance)
 	
+	emisorreset.button_down.connect(_delete)
 	pass
 
 func _count_new_instance():
 	instancesCount += 1 
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	
+func _delete():
+	instancesCount = 0
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	
